88f369f1b6bfedb96c9b952c44d0d97aa7436913
Oscar <Oscar@DESKTOP-R1JR8A2>
2017-06-01 19:54:30 -0500

    d

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -38,7 +38,7 @@ public class ChartServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("image/png");
         OutputStream outputStream = response.getOutputStream();
         JFreeChart chart = getChart();
@@ -47,57 +47,17 @@ public class ChartServlet extends HttpServlet {
         ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
         
         
-        response.setContentType("image/png");
-        OutputStream outputStream2 = response.getOutputStream();
-        JFreeChart chart2 = getChart2();
-        int width2 = 500;
-        int height2 = 350;
-        ChartUtilities.writeChartAsPNG(outputStream2, chart2, width2, height2);
-
+        
     }
 
     public JFreeChart getChart() {
-        ColmenaDAO d = new ColmenaDAO();
-        
-        ArrayList<colmena> col = (ArrayList<colmena>) d.consultaGrafico2();
-        
-        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-        for (int i = 0; i < col.size(); i++) {
-            dataset.setValue(col.get(i).getKilosMiel(), "colmena "+col.get(i).getId_colmena()+1, "Kilos colmena"+col.get(i).getId_colmena()+1);
-        }
-        JFreeChart chart = ChartFactory.createBarChart3D(
-                "3D Bar Chart Demo", // chart title
-                "Category", // domain axis label
-                "Value", // range axis label
-                dataset, // data
-                PlotOrientation.VERTICAL, // orientation
-                true, // include legend
-                true, // tooltips
-                false // urls
-        );
-
-        CategoryPlot plot = chart.getCategoryPlot();
-        CategoryAxis axis = plot.getDomainAxis();
-        axis.setCategoryLabelPositions(
-                CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 8.0)
-        );
-
-        CategoryItemRenderer renderer = plot.getRenderer();
-        renderer.setItemLabelsVisible(true);
-        BarRenderer r = (BarRenderer) renderer;
-        r.setMaximumBarWidth(0.05);
-        return chart;
-
-    }
-    
-    public JFreeChart getChart2() {
-        ColmenaDAO d = new ColmenaDAO();
+        ColmenaDAO dAO = new ColmenaDAO();
         //Crear la capa de servicios que se enlace con el DAO
-        ArrayList<colmena> col = (ArrayList<colmena>) d.findAll();
+        ArrayList<colmena> col = (ArrayList<colmena>) dAO.findAll();
         DefaultPieDataset dataset = new DefaultPieDataset();
 
         for (int i = 0; i < col.size(); i++) {
-            double porcentaje = (col.get(i).getPanalesConAlimento()/ 10) * 100;
+            int porcentaje = (col.get(i).getPanalesConAlimento()/ 10) * 100;
             dataset.setValue("colmena 1", porcentaje);
             dataset.setValue("otros", 100 - porcentaje);
 
@@ -118,5 +78,7 @@ public class ChartServlet extends HttpServlet {
 
         return chart;
     }
+    
+
 
 }
